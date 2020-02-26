01d49c49f792c30ee8e0012b927fb6dca2cf7bfe
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-02-26 13:55:47 +0000

    Commiting to upload files in github repository

--- assets/js/slider/slider_native_script/dianaSlider_check_settings.js
+++ assets/js/slider/slider_native_script/dianaSlider_check_settings.js
@@ -3,15 +3,22 @@ import {DianaSliderMessageBox} from './dianaSlider_message.js'; // Imprting clas
 // Defining private variables
 const _checkElement = new WeakMap();
 const _checkStatus = new WeakMap();
-
+const _checkBackgroundImages = new WeakMap();
 const dianaMsg = new DianaSliderMessageBox(); // initialising message object 
 
 // Creating checking class
 export class CheckSettings{
 
     constructor(){
+        
+        // Getting dianaSlider's div element
+        const dianaEl = document.getElementById("dianaSlider");
+
+        _checkBackgroundImages.set(this, Number(dianaEl.style.backgroundImage.length));
+        console.log(_checkBackgroundImages.get(this));
+
         // Defining private variables and setting values
-        _checkElement.set(this, document.getElementById("dianaSlider"));
+        _checkElement.set(this, dianaEl);
         _checkStatus.set(this, false); 
 
         // Checking if element is div html element, if it has #dianaSlider
@@ -20,7 +27,9 @@ export class CheckSettings{
 
         else if(_checkElement.get(this).tagName != "DIV")
             dianaMsg.getMessage("dianaSlider's wrapper must be a div html element");
-        else 
+        else if(_checkBackgroundImages.get(this) === 0)
+            dianaMsg.getMessage("Please set background images to dianaSlider's div element");
+        else
             // if all reauired settigns are ok then setting true value   
              _checkStatus.set(this, true);   
     }
@@ -29,6 +38,5 @@ export class CheckSettings{
     get settingsStatus(){
         return _checkStatus.get(this);
     }
-
    
 }
--- assets/js/slider/slider_native_script/index.js
+++ assets/js/slider/slider_native_script/index.js
@@ -4,3 +4,7 @@ import {CheckSettings} from './dianaSlider_check_settings.js';
 const setting = new CheckSettings();
 
 console.log(setting.settingsStatus);
+
+var full = location.pathname;
+var path = full.substr(full.lastIndexOf("/") + 1);
+console.dir(full);
