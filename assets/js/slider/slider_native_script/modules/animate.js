c5c78d16017518a6c933a18345ab1a3185fb37e3
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-03-18 15:29:18 +0000

    Animating width and height for background image

--- assets/js/slider/slider_native_script/modules/animate.js
+++ assets/js/slider/slider_native_script/modules/animate.js
@@ -5,10 +5,11 @@ const settings = new CheckSettings();
 const conf = new Config();
 
 
+
 export class Animate{
 
     constructor(){
-
+        
         // Checking if all reuqried setting are ok
         if(settings.settingsStatus){
             
@@ -19,23 +20,52 @@ export class Animate{
             let imgFullUrl = dom + folderUrl + imgName;
 
             // Getting image's current width and height
-            let dimension, image;
+            // This part of the code i found and copyed from here:
+            // https://stackoverflow.com/questions/3098404/get-the-size-of-a-css-background-image-using-javascript
+            let dimension = {
+                width: 0,
+                height: 0
+            }
+
+            let image;
             image = new Image();
             image.src = imgFullUrl;
+            
+
+            // On load getting width and height of image
             image.onload = function() {
+
                 dimension = {
                     width: image.naturalWidth,
                     height: image.naturalHeight
                 };
-                console.log(dimension);
-            }; 
+               
+              
+                // Starting animation
+                    setInterval(function(){
+
+                        let scaledUpWidth = dimension.width++;
+                        let scaleUpHight = dimension.height++;
+
+                        document.getElementById("dianaSlider").style.backgroundSize = `${scaledUpWidth}px ${scaleUpHight}px`;
+                        
+                    },15);
+
+
+
+                
+               
+            };
             
-            
+           
+        
 
         }
 
         
     }
+
+    
 
     getImagesLengths(w, h){
         let ww = w;
