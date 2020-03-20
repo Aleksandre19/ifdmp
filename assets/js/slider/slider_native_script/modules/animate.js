6d7e2f3b499c85be2c7262fb63e4a3838a5bf129
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-03-20 15:17:02 +0000

    Breaking down a Animation code  on to seperate functions

--- assets/js/slider/slider_native_script/modules/animate.js
+++ assets/js/slider/slider_native_script/modules/animate.js
@@ -1,77 +1,74 @@
+// Importing classes
 import {CheckSettings} from './check_settings.js';
 import {Config} from '../config.js';
 
+// Initialising objects
 const settings = new CheckSettings();
 const conf = new Config();
 
+// Declaring private variabels with WeakMap();
+const _imageNaturalWidth = new WeakMap();
+const _imageNaturalHeight = new WeakMap();
 
-
+// Creating Animation's class
 export class Animate{
 
     constructor(){
-        
         // Checking if all reuqried setting are ok
         if(settings.settingsStatus){
-            
-            // Creating image's full path    
-            let dom = window.location.href;
-            let folderUrl = 'assets/js/slider/slider_images/lg/';
-            let imgName = conf.imgName[1];
-            let imgFullUrl = dom + folderUrl + imgName;
+            Animate.getNaturalDiminsion();
+        }
+    }
 
+    
+    // Getting natural dimentions of image
+
+    static getNaturalDiminsion(){
             // Getting image's current width and height
             // This part of the code i found and copyed from here:
             // https://stackoverflow.com/questions/3098404/get-the-size-of-a-css-background-image-using-javascript
-            let dimension = {
-                width: 0,
-                height: 0
-            }
-
             let image;
             image = new Image();
-            image.src = imgFullUrl;
+            image.src = Animate.imageFullPath(conf.imgName[1]);
             
 
             // On load getting width and height of image
             image.onload = function() {
 
-                dimension = {
-                    width: image.naturalWidth,
-                    height: image.naturalHeight
-                };
-               
-              
-                // Starting animation
-                    setInterval(function(){
+                // Setting natural dimentions to the variables
+                _imageNaturalWidth.set(this, image.naturalWidth);
+                _imageNaturalHeight.set(this, image.naturalHeight);
 
-                        let scaledUpWidth = dimension.width++;
-                        let scaleUpHight = dimension.height++;
-
-                        document.getElementById("dianaSlider").style.backgroundSize = `${scaledUpWidth}px ${scaleUpHight}px`;
-                        
-                    },15);
-
-
-
-                
-               
+                // Calling animation function
+                Animate.startAnimation(_imageNaturalWidth.get(this), _imageNaturalHeight.get(this));
+                    
             };
-            
-           
-        
-
-        }
-
-        
     }
 
     
-
-    getImagesLengths(w, h){
-        let ww = w;
-        let hh = h;
-        console.log(ww);
+    // Getting image's full path
+    static imageFullPath(imgNames){   
+            let dom = window.location.href;
+            let folderUrl = 'assets/js/slider/slider_images/lg/';
+            let imgName = imgNames;
+            let imgFullUrl = dom + folderUrl + imgName;
+            return imgFullUrl;
     }
+
+
+    // Starting animation
+    static startAnimation(natWidth, natHeight){
+        
+            setInterval(() => {
+
+                let scaledUpWidth = natWidth++;
+                let scaleUpHight = natHeight++;
+
+                document.getElementById("dianaSlider").style.backgroundSize = `${scaledUpWidth}px ${scaleUpHight}px`;
+                        
+           },15);
+    }
+   
        
 
 }
