7627759d120a7c7c61c557e6f1e8527b3233ca59
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-04-03 13:02:35 +0000

    Writting calculateImageIncreasedDemision() function in image_animation.js file

--- assets/js/slider/slider_native_script/modules/image_animation.js
+++ assets/js/slider/slider_native_script/modules/image_animation.js
@@ -3,6 +3,7 @@
 // Defining private variables by using WeakMap()
 
 const _imageStartingNaturLWidth = new WeakMap();
+const _functionHasCalled = new WeakMap();
 
 export class ImageAnimation{
 
@@ -26,7 +27,7 @@ export class ImageAnimation{
              _imageStartingNaturLWidth.set(this, natW);
 
            // Defining variable to check if function has been called   
-            //_functionHasCalled.set(this, true);
+            _functionHasCalled.set(this, true);
 
             
             setInterval(() => {
@@ -34,10 +35,20 @@ export class ImageAnimation{
                 let scaledUpWidth = natW++;
 
                 document.getElementById(elDivId).style.backgroundSize = `${scaledUpWidth}px`;
-            
+
                 // When image's dimention is increased by 7% going on next step
+                if(Math.round(this.calculateImageIncrisedDimension(scaledUpWidth)) === 7 && _functionHasCalled.get(this)){
+           
+                    console.log("Has increased")
+
+                    _functionHasCalled.set(this, false);
+                }
                
            },15);
+    }
+
+    calculateImageIncrisedDimension(sUpWidth){
+        return (sUpWidth - _imageStartingNaturLWidth.get(this)) / _imageStartingNaturLWidth.get(this) * 100;
     }
 
 }
