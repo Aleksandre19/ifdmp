54d37c196933c8dedcc5b258f6b09f43fe62d799
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-04-17 12:34:40 +0000

    Animating first image's text by writting imageTextAnimation() in animation_text.js

--- assets/js/slider/slider_native_script/modules/animation_text.js
+++ assets/js/slider/slider_native_script/modules/animation_text.js
@@ -3,14 +3,17 @@ import {Config} from '../config.js';
 const imgText = new Config();
 
 export class AnimationTexts{
+    
     constructor(){
+
         this.imageText = function(img, desc){
             return imgText.imageTexts[img][desc];
         }
         
-        console.log(this.imageText(0,0));
+        this.animateText = null;
 
         this.insertImageTextsInDiv("text-f", "author-f", 0);
+        this.imageTextAnimation("text-f");
     }
 
 
@@ -21,5 +24,29 @@ export class AnimationTexts{
 
         text.innerHTML = `${this.imageText(imgOrder, 0)}`;
         author.innerHTML = `${this.imageText(imgOrder, 1)}`;
+    }
+
+
+    imageTextAnimation(id){
+
+        let left = -150;
+
+        this.animateText = setInterval(() => {
+            
+            if(left.toFixed(0) >= 300){
+                clearInterval(this.animateText);
+                left = -150;
+            } else if(left.toFixed(0) <= 0){
+                left += 0.7;
+            }else if(left.toFixed(0) >= 13){
+                left += 0.9;
+            }else{
+                left += 0.01;
+            }
+           
+            document.getElementById(id).style.transform = `translate(${left}%)`;
+
+        });
+
     }
 }
