5ef6fe2c129f13f37401ab1f75d16abc69d3390a
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-05-15 16:40:18 +0000

    Commenting index.js

--- assets/js/slider/slider_native_script/index.js
+++ assets/js/slider/slider_native_script/index.js
@@ -1,40 +1,25 @@
-import {AnimationManager} from './modules/animation_manager.js';
-import {AnimationTexts} from './modules/animation_text.js';
+    /**
+     * This is the index page and animation starts here.
+     * It imports two main objects and initiates them
+     */
 
-export class AnimationIndex{
-    constructor(){
-        const an = new AnimationManager();
-        const text = new AnimationTexts();
+    // Importing files 
+    import {AnimationManager} from './modules/animation_manager.js';
+    import {AnimationTexts} from './modules/animation_text.js';
+
+    /**
+     * This class initiates animation manager's and animation text's objects
+     */
+    export class AnimationIndex{
+        constructor(){
+            const an = new AnimationManager();
+            const text = new AnimationTexts();
+        }
     }
-}
 
-
-
+    // Initiating index object
     const index = new AnimationIndex();
 
-
-//import {DianaSliderMessageBox} from './modules1/message.js';
-//import {CheckSettings} from './modules1/check_settings.js';
-//import {Images} from './modules1/images.js';
-//import {Config} from './config.js';
-//import {Animate} from './modules1/animate.js';
-
-//const setting = new CheckSettings();
-
-//console.log(setting.settingsStatus);
-
-//var full = location.pathname;
-//var path = full.substr(full.lastIndexOf("/") + 1);
-//console.dir(full);
-
-//const conf = new Config();
-//console.log(conf.imgName);
-
-//const img = new Images();
-
-//console.log(img.imagesQuantityStatus);
-
-//const ani = new Animate();
 
 
 
