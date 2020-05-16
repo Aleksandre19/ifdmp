2ded4cc30e58aac09261a27786305c22ae08335d
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-05-16 15:43:39 +0000

    Removing webpack from github

--- assets/js/slider/webpack/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-    entry: './slider_native_script/index.js',
-    output: {
-        filename: 'slider.js',
-        path: path.resolve(__dirname, "production")
-    }
-};
